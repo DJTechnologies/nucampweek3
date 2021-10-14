155c0323c454492fbde37a6dc8c75f2ad8b82110
MJ <Juliana.technologies.83@gmail.com>
2021-10-14 18:46:52 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+// import { Navbar, NavbarBrand } from "reactstrap";
+import "./App.css";
+// import Directory from "./components/DirectoryComponent";
+
+// import { CAMPSITES } from "./shared/campsites";
+import Main from "./components/MainComponent";
+
+class App extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     campsites: CAMPSITES,
+  //   };
+  // }
+  render() {
+    return (
+      <div className="App">
+        <Main />
+      </div>
+    );
+  }
+}
+
+export default App;
