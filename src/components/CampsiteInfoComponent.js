3181d1febb2abf0929ce047c196df9a593d3c9e5
MJ <Juliana.technologies.83@gmail.com>
2021-10-14 20:42:20 -0400

    footer and header

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,14 +1,13 @@
-import { render } from "@testing-library/react";
 import React, { Component } from "react";
 import {
   Card,
   CardImg,
+  CardImgOverlay,
   CardText,
   CardBody,
   CardTitle,
-  Breadcrumb,
-  BreadcrumbItem,
 } from "reactstrap";
+
 class CampsiteInfo extends Component {
   renderCampsite(campsite) {
     return (
@@ -29,36 +28,101 @@ class CampsiteInfo extends Component {
       return (
         <div className="col-md-5 m-1">
           <h4>Comments</h4>
-          {comments.map((comment) => {
-            return (
-              <div key={comment.id} style={{ marginBottom: "20px" }}>
-                <div> {comment.text} </div>
-                --{comment.author} {}
-                {new Intl.DateTimeFormat("en-US", {
-                  year: "numeric",
-                  month: "short",
-                  day: "2-digit",
-                }).format(new Date(Date.parse(comment.date)))}
-              </div>
-            );
-          })}
+          {comments.map((comments) => (
+            <div className="p-1" key={comments.id}>
+              {comments.text}
+              <br></br>
+              -- {comments.author},{" "}
+              {new Intl.DateTimeFormat("en-US", {
+                year: "numeric",
+                month: "short",
+                day: "2-digit",
+              }).format(new Date(Date.parse(comments.date)))}
+            </div>
+          ))}
         </div>
       );
+    } else {
+      return <div></div>;
     }
-    return <div />;
   }
+
   render() {
     if (this.props.campsite) {
       return (
-        <div className="container">
-          <div className="row">
-            {this.renderCampsite(this.props.campsite)}
-            {this.renderComments(this.props.campsite.comments)}
-          </div>
+        <div className="row">
+          {this.renderCampsite(this.props.campsite)}
+          {this.renderComments(this.props.campsite.comments)}
         </div>
       );
+    } else {
+      return <div></div>;
     }
-    return <div />;
   }
 }
+
 export default CampsiteInfo;
+// import { render } from "@testing-library/react";
+// import React, { Component } from "react";
+// import {
+//   Card,
+//   CardImg,
+//   CardText,
+//   CardBody,
+//   CardTitle,
+//   Breadcrumb,
+//   BreadcrumbItem,
+// } from "reactstrap";
+// class CampsiteInfo extends Component {
+//   renderCampsite(campsite) {
+//     return (
+//       <div className="col-md-5 m-1">
+//         <Card>
+//           <CardImg top src={campsite.image} alt={campsite.name} />
+//           <CardBody>
+//             <CardTitle>{campsite.name}</CardTitle>
+//             <CardText>{campsite.description}</CardText>
+//           </CardBody>
+//         </Card>
+//       </div>
+//     );
+//   }
+
+//   renderComments(comments) {
+//     if (comments) {
+//       return (
+//         <div className="col-md-5 m-1">
+//           <h4>Comments</h4>
+//           {comments.map((comment) => {
+//             return (
+//               <div key={comment.id} style={{ marginBottom: "20px" }}>
+//                 <div> {comment.text} </div>
+//                 --{comment.author} {}
+//                 {new Intl.DateTimeFormat("en-US", {
+//                   year: "numeric",
+//                   month: "short",
+//                   day: "2-digit",
+//                 }).format(new Date(Date.parse(comment.date)))}
+//               </div>
+//             );
+//           })}
+//         </div>
+//       );
+//     }
+//     return <div />;
+//   }
+//   render() {
+//     if (this.props.campsite) {
+//       return (
+//         <div className="container">
+//           <div className="row">
+//             {this.renderCampsite(this.props.campsite)}
+//             {this.renderComments(this.props.campsite.comments)}
+//           </div>
+//         </div>
+//       );
+//     }
+//     return <div />;
+//   }
+// }
+// export default CampsiteInfo;
